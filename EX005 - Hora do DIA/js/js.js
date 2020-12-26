817b52b6c7390a19dbc62f5a7599fd02838d90ef
Marcos Dias <59514474+MDias13@users.noreply.github.com>
2020-12-26 11:00:35 -0300

    :package:  EX005 HORA DO DIA

---
+++ EX005 - Hora do DIA/js/js.js
@@ -0,0 +1,32 @@
+function carregar(){
+let msg = window.document.querySelector('.msg');
+
+let foto = window.document.querySelector('.foto img');
+
+let data = new Date();
+let hora = data.getHours();
+let min = data.getMinutes();
+
+if(hora > 6 && hora < 12){
+    msg.innerHTML = ` Bom Dia , Agora São ${hora} Horas e ${min} min.`
+
+    foto.src = 'img/manha.jpg.png'
+    document.body.style.background = '#B2845C'
+    
+
+}else if(hora > 12 && hora <18){
+    msg.innerHTML = ` Boa Tarde , Agora São ${hora} Horas e ${min} min.`
+
+    foto.src = 'img/tarde.png'
+    document.body.style.background = 'grenn'
+    
+} else{
+    msg.innerHTML = ` Boa Noite , Agora São ${hora} Horas e ${min} min.`
+
+    foto.src = 'img/Noite.png'
+    document.body.style.background = 'blue'
+    
+
+}
+    
+}
